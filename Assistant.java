b20fc364c8cc9a80063edf34f000b22ff09b4282
ieatyourshushi <69708951+ieatyoursushi@users.noreply.github.com>
2022-11-11 12:23:47 +0000

    Latest

--- Assistant.java
+++ Assistant.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
 
 public class Assistant {
     Assistant() {
         
     }
+    public static void timedMsg(String msg, int milliseconds) throws InterruptedException {
+        for(int i = 0; i < msg.length(); i++) {
+            System.out.print(msg.charAt(i));
+            Thread.sleep(milliseconds);
+        }
+    }
     public static void call(Scanner input) throws InterruptedException {
-        System.out.println("Please select a catagory below: \nAsk a question [q]\nView products[p]\nMake a suggestion [s] ");
+        String options = "Please select a catagory below: ";
+        String category = "\nAsk a question [q]\nView products[p]\nMake a suggestion [s] ";
+        timedMsg(options, 50);
+        timedMsg(category, 10);
+        System.out.println();
+        
         String userInp = input.nextLine();
         if(userInp.equals("q")) {
-            System.out.println("Ask away");
+            timedMsg("Ask Away: ", 50);
             String questio = input.nextLine();
         } else if (userInp.equals("p")) {
             System.out.println("Products: duct tape");
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import java.io.*;
 //goal: imitate python methods
+ 
 public class Main{
     public static void countDownPause(int seconds, int milliseconds) throws InterruptedException{
         System.out.print("Waking in ");
@@ -12,7 +13,7 @@ public class Main{
         }
     }
     //wip loading screen
-    public static void tree(int row) {
+    public static void tree(int row) throws InterruptedException {
         for(int i = 0; i < row; i++) {
             for(int j = row-i; j > -1; j--){
                 System.out.print(" ");
@@ -21,18 +22,20 @@ public class Main{
                 System.out.print("* ");
             }
             System.out.println();
+            Thread.sleep(250);
         }
     }
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
-        countDownPause(3, 1000);
-        tree(10);
+        tree(5);
         while(true) {
             Assistant.call(input);
         }
     }
+    
 } 
 class Product {
+    public static final String exp = "what";
     public double price;
     Product(double price) {
         this.price = price;
